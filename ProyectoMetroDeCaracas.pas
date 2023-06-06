6e074f8a0ed80ad4b2d49feeaf6e047b216b19e8
Juan SanchezVegas <juansancheez98@gmail.com>
2023-06-05 23:05:21 -0400

    Case para preguntar datos

--- ProyectoMetroDeCaracas.pas
+++ ProyectoMetroDeCaracas.pas
@@ -1,5 +1,24 @@
-program Metro de Caracas;
+program MetroDeCaracas;
+uses crt;
+var sino, n, a, ci, cant:integer;
 
 begin
+writeln ('Sea usted bienvenido al Metro de Caracas');
+writeln ('¿Desea usted comprar un boleto?');
+writeln ('1- Si            2-No');
+readln (sino);
+	case sino of 
+		1:
+		begin
+		writeln ('Ingrese su nombre por favor');
+		readln (n);
+		writeln ('Ingrese su apellido por favor');
+		realn (a);
+		writeln ('Ingrese su cedula de identidad por favor');
+		readln (ci);
+		
+		
+
+
 
 end.
